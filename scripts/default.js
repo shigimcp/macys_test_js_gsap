774c93f4d6036f14838a59436fd1496c56d15d3e
shigimcp <shigimcp@hotmail.com>
2019-07-24 11:11:28 -0400

    INITIAL js/gsap version (please see github project, macys_test, for outstanding issues)

---
+++ scripts/default.js
@@ -0,0 +1,74 @@
+'use strict';
+
+//<!--/* ========================= INITIALIZE PAGE ========================= */-->
+
+function pageInit() {
+	window.scrollTo(0, 0);
+}
+
+
+//==================== FUNCTION: getDims(thisElem) ====================
+
+function getDims(thisElem) {
+
+	var thisX = document.getElementById(thisElem).getBoundingClientRect().x;
+	var thisY = document.getElementById(thisElem).getBoundingClientRect().y;
+	var thisW = document.getElementById(thisElem).getBoundingClientRect().width;
+	var thisH = document.getElementById(thisElem).getBoundingClientRect().height;
+
+	return [thisX, thisY, thisW, thisH];
+}
+
+
+//==================== FUNCTION: swapBG(thisImg) ====================
+
+function swapBG(thisElem, thisImgURL) {
+
+	if (thisImgURL == 'none') {
+		TweenMax.to(thisElem, 1, {backgroundImage:'none'});
+	} else {
+		TweenMax.to(thisElem, 1, {backgroundImage:'url(' + thisImgURL + ')'});
+	}
+}
+
+
+//==================== FUNCTION: scrollToActive(thisNavItem) ====================
+
+function scrollToActive(thisNavItem) {
+
+	var scrollDuration = 0.5;
+	var thisAnchor;
+
+	var thisNavItemX = getDims(thisNavItem.id)[0] - getDims("navContainer01")[0];
+
+
+	switch(thisNavItem.id) {
+
+		case 'navItem01':
+			thisAnchor = "#triAnchor01";
+			break;
+
+		case 'navItem02':
+			thisAnchor = "#triAnchor02";
+			break;
+
+		case 'navItem03':
+			thisAnchor = "#triAnchor03";
+			break;
+
+		case 'navItem04':
+			thisAnchor = "#postPregAnchor01";
+			break;
+
+		default:
+			console.log('Move along folks! Nothing to see here...');
+	}
+
+
+	var scrollTL = new TimelineMax({delay:0});
+
+	scrollTL
+		.to(NAV_btn_active, scrollDuration, {x:thisNavItemX, ease:Power3.easeOut}, "frame01 +=0")
+		.to(window, scrollDuration, {scrollTo:{y:thisAnchor, autoKill:false}, ease:Power3.easeOut}, "frame01 +=0")
+	;
+}
